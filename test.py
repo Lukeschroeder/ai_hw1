cbf5be9c97c72b2d73bce52cd30146e508783030
Luke Hodgson Schroeder <Lukeschroeder@Lukes-MBP-2.home>
2020-02-17 23:39:50 -0500

    Added function to put label

--- test.py
+++ test.py
@@ -5,14 +5,11 @@ import random
 
 n = 15
 
-
 class App(tk.Tk):
     def __init__(self, *args, **kwargs):
         tk.Tk.__init__(self, *args, **kwargs)
         self.canvas = tk.Canvas(self, width=n*25, height=n*25, borderwidth=0, highlightthickness=0)
         self.canvas.pack(side="top", fill="both", expand="true")
-        self.rows = 100
-        self.columns = 100
         self.cellwidth = 25
         self.cellheight = 25
 
@@ -23,9 +20,18 @@ class App(tk.Tk):
                 y1 = row * self.cellheight
                 x2 = x1 + self.cellwidth
                 y2 = y1 + self.cellheight
-                self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="white", tags="rect")
-                self.canvas.create_text(((x1 + x2) / 2, (y1 + y2) /2), text="a2")
+                self.rect[row,column] = self.canvas.create_rectangle(x1,y1,x2,y2, fill="grey", tags="rect")
+
+    def addlabel(self, x, y,label):
+        xcoord = (x - 1) * self.cellwidth + self.cellwidth / 2
+        ycoord = (y - 1) * self.cellheight + self.cellheight / 2
+        self.canvas.create_text((xcoord, ycoord), text=label)    
+        self.canvas.update()          
+    
 
 if __name__ == "__main__":
     app = App()
+    app.addlabel(2,7,"32")
     app.mainloop()
+    
+    
